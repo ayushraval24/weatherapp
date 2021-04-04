062864efa1305cf1ccac3ae74acb52931506c2ab
Ayush <omraval24@gmail.com>
2021-04-04 11:11:54 +0530

    deploying on ghpages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+let api = {
+  key: "d5d483e8405ac237ceaee3f997bea178",
+  baseURL: "https://api.openweathermap.org/data/2.5/"
+}
+
+let tem;
 
 function App() {
+
+  let [input, setInput] = useState("");
+  let [city, setCity] = useState("Delhi");
+  let [country, setCountry] = useState("India");
+  let [temp, setTemp] = useState("35");
+  let [tempMax, setTempMax] = useState("45");
+  let [tempMin, setTempMin] = useState("25");
+  let [weatherType, setWeatherType] = useState("Sunny");
+
+  function inputChange(e) {
+    setInput(e.target.value)
+  }
+
+  function setQuery(e) {
+    if (e.key == "Enter") {
+      getResult(input);
+    }
+  }
+
+  function getResult(val) {
+    fetch(`${api.baseURL}weather?q=${val}&units=metric&appid=${api.key}`)
+      .then(weather => {
+        setInput("");
+        return weather.json()
+      })
+      .then(displayResult).catch(() => { window.alert("Please Enter proper Input") })
+  }
+
+  function displayResult(weather) {
+    tem = weather.main.temp;
+    console.log(tem);
+    console.log(typeof (tem))
+    setCity(weather.name);
+    setCountry(weather.sys.country)
+    setTemp(weather.main.temp)
+    setTempMax(weather.main.temp_max)
+    setTempMin(weather.main.temp_min)
+    setWeatherType(weather.weather[0].main)
+  }
+
+  let d = new Date();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={tem < 25 ? "App" : "warm"}>
+      <main className="wrapApp">
+        <div className="wrapper">
+          <section className="head">
+            <input id="input" className="search-box" placeholder="Select city..." type="text" value={input} onChange={inputChange} onKeyPress={setQuery} />
+            <div className="country">{city}, {country}</div>
+            <div className="date">{d.toDateString()}</div>
+          </section>
+          <section className="weather">
+            <div className="temp">{temp} °C</div>
+            <div className="weather-type">{weatherType}</div>
+            <div className="high-low">{tempMin}°C / {tempMax}°C  </div>
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
